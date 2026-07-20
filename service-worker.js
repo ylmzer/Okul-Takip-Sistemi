22a5c6f2325ac92ab08913a2720f8262b54bd17d
YILMAZ ER <e.yilmazer06@gmail.com>
2026-07-20 22:08:07 +0300

    Mobil modül başlığını normal akışa al

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "ots-pwa-v46";
+const CACHE_NAME = "ots-pwa-v47";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css?v=81",
-  "./theme-overrides.css?v=105",
+  "./theme-overrides.css?v=106",
   "./scripts/modules/student-tracking/styles.css?v=1",
   "./app.js?v=102",
   "./icons/logo-clean.svg",
